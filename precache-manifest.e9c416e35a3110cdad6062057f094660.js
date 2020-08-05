817503c1bff04ad20fe9fb621413e2d0461236b5
Becca Staple <petite.becca.s@gmail.com>
2020-08-05 10:38:06 +0100

    Updates

---
+++ precache-manifest.e9c416e35a3110cdad6062057f094660.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "affa9c124fb463a42a44a056d1c0629d",
+    "url": "/n7-pastry/index.html"
+  },
+  {
+    "revision": "e709f2dd57489a9ec6b9",
+    "url": "/n7-pastry/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "4f91d20d01cb4c866664",
+    "url": "/n7-pastry/static/css/main.d8ba7109.chunk.css"
+  },
+  {
+    "revision": "e709f2dd57489a9ec6b9",
+    "url": "/n7-pastry/static/js/2.b2b2dd98.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/n7-pastry/static/js/2.b2b2dd98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f91d20d01cb4c866664",
+    "url": "/n7-pastry/static/js/main.06cf72f7.chunk.js"
+  },
+  {
+    "revision": "77215c8f63ad4571f8dd",
+    "url": "/n7-pastry/static/js/runtime-main.7ac365b3.js"
+  },
+  {
+    "revision": "a417abedaee52ce5435bfd155c0959ed",
+    "url": "/n7-pastry/static/media/N7-logo-v4-clipped.a417abed.png"
+  },
+  {
+    "revision": "f24ea3f6c6a7622d4db32a5a8b0db9d2",
+    "url": "/n7-pastry/static/media/blueberry-cake-v2.f24ea3f6.jpg"
+  },
+  {
+    "revision": "12c630698807425a23c4a56d7caa0b47",
+    "url": "/n7-pastry/static/media/blueberry-cake.12c63069.png"
+  },
+  {
+    "revision": "f5cae9b99f144956f7a798983c0a0991",
+    "url": "/n7-pastry/static/media/blueberry-cake2.f5cae9b9.jpg"
+  },
+  {
+    "revision": "e3d5928dde8b39d714b7bcd03ff1befa",
+    "url": "/n7-pastry/static/media/brown-cake.e3d5928d.jpg"
+  },
+  {
+    "revision": "1eab66309477206960ef59a529678267",
+    "url": "/n7-pastry/static/media/chocoCake.1eab6630.jpg"
+  },
+  {
+    "revision": "0f9f45be6d49ea1ac567761b967ac76f",
+    "url": "/n7-pastry/static/media/strawberry-cake.0f9f45be.jpg"
+  }
+]);
